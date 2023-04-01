f4deb6b40fc04a79d44f48eb0ebcf11cc7f2ea5a
vaderlock <smuntase@nyit.edu>
2023-04-01 01:31:54 -0400

    first commit w/ next.js + thirdweb components

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,31 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { ConnectWallet, Web3Button, ThirdwebNftMedia, useAddress, useContract } from "@thirdweb-dev/react";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+    // Connect to your NFT contract
+    // const { contract } = useContract("{{contract_address}}");
+    // Load the NFT metadata from the contract using a hook
+    // const { data: nft, isLoading, error } = useNFT(contract, "0");
+
+    // Render the NFT onto the UI
+    // if (isLoading) return <div>Loading...</div>;
+    // if (error || !nft) return <div>NFT not found</div>;
+
+    const address = useAddress();
+
+    function WalletAddress({ address }) {
+      if (!address) {
+        return <div>No wallet connected</div>;
+      }
+    
+      return <div>My wallet address is {address}</div>;
+    }
+
   return (
     <>
       <Head>
@@ -15,108 +36,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
+      {/* <Web3Button
+      contractAddress="{{contract_address}}"
+      action={async (contract) => contract.call("myFunctionName")}
+    >
+      Call myFunctionName from the connected wallet
+      </Web3Button> */}
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+      <ConnectWallet
+      theme="dark"
+      btnTitle="Connect Wallet"
+    />
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
+    {/* <Web3Button
+      contractAddress="0x..."
+      action={(contract) => console.log(contract)} // Logic to execute when clicked
+      onSubmit={() => console.log("Transaction submitted")} //Callback function to be run after the user has confirmed the transaction.
+      onSuccess={(result) => alert("Success!")} //Callback function to be run when the contract method call is successful.
+      onError={(error) => alert("Something went wrong!")} //Callback function to be run when the contract method call fails.
+    >
+      Create Social Profile
+    </Web3Button> */}
 
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
+    {/* <ThirdwebNftMedia 
+    metadata={nft.metadata} 
+    width={500}
+    height={500}
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+    /> */}
+
+    <WalletAddress address={address} />
+
       </main>
     </>
   )
