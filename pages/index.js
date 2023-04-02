38011143d25b1d71635a5c58ff079377829e1187
willschneider15 <willschneider15@gmail.com>
2023-04-02 02:10:27 -0400

    pushed fix

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
             mouseControls: false,
             touchControls: false,
             gyroControls: false,
-            minHeight: 200.00,
+            minHeight: 1000.00,
             minWidth: 200.00,
             highlightColor: 0x2d2d2d,
             midtoneColor: 0x7f05,
@@ -80,119 +80,126 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/sword.png" />
       </Head>
-      
-      <nav className='fixed z-20'>
-          <Image src="/Nomad.png" alt="logo" width={200} height={200} />
-      </nav>
 
-      <main className="flex flex-col w-full flex-1 items-center justify-between px-20 min-h-full h-screen text-center" ref={vantaRef}>
-   
+      <div className=" min-h-screen bg-cover" ref={vantaRef}>
 
-       <div className='items-center justify-center m-auto'>
-         <div className="grid grid-cols-1 text-center pt-40 mx-5 text-center m-auto">
-          <div className="max-w-md m-auto">
+        <nav className='fixed z-20'>
+            <Image src="/Nomad.png" alt="logo" width={200} height={200} />
+        </nav>
 
-            <h1 className="text-3xl font-bold pb-10 text-white">Nomad</h1>
+        <main className="flex flex-col w-full flex-1 items-center justify-between px-20 h-screen text-center" >
+    
 
-            <div className='grid grid-cols-1 gap-10'>
+        <div className='items-center justify-center m-auto'>
+          <div className="grid grid-cols-1 text-center pt-40 mx-5 text-center m-auto">
+            <div className="max-w-md m-auto">
 
-            <ConnectWallet
-              theme="dark"
-              btnTitle="Connect Wallet"
-              className='my-10 px-10'
-            />
+              <h1 className="text-3xl font-bold pb-10 text-white">Nomad</h1>
 
-            {address && (
-              <button className="bg-gradient-to-r from-gray-700 to-indigo-900 hover:from-indigo-900 hover:to-gray-700 text-white py-2 px-4 rounded">
-               Create social identity
-              </button>
-            )}
+              <div className='grid grid-cols-1 gap-10'>
 
-            </div>
-          
-        {address && (
-          <div className="my-10 text-white">
-            <label htmlFor="txHash" className="block mb-2">
-              Enter transaction hash:
-            </label>
-            <div className="flex items-center">
-              <input
-                id="txHash"
-                type="text"
-                value={txHash}
-                onChange={handleTxHashChange}
-                className="flex-1 mr-5 px-2 py-2 border rounded-lg"
+              <ConnectWallet
+                theme="dark"
+                btnTitle="Connect Wallet"
+                className='my-10 px-10'
               />
 
-            </div>
-            {status ? (
-              <div className="mt-4">
-                <p>Status: {status.status}</p>
-                {status.gasPaidInfo && (
-                  <p>Gas Paid Status: {status.gasPaidInfo.status}</p>
-                )}
-                {status.errors && <p>Errors: {JSON.stringify(status.errors)}</p>}
-                {status.callData && (
-                  <p>Call Data: {JSON.stringify(status.callData)}</p>
-                )}
+              {address && (
+                <button className="bg-gradient-to-r from-gray-700 to-indigo-900 hover:from-indigo-900 hover:to-gray-700 text-white py-2 px-4 rounded">
+                Create social identity
+                </button>
+              )}
+
               </div>
-            ) : (
-              <p className="mt-4">Enter a transaction hash to see its status</p>
-            )}
-          </div>
-        )}
-          
-          <div className="my-10 text-white">
-              <WalletAddress address={address} />
-          </div>
-      
+            
+          {address && (
+            <div className="my-10 text-white">
+              <label htmlFor="txHash" className="block mb-2">
+                Enter transaction hash:
+              </label>
+              <div className="flex items-center">
+                <input
+                  id="txHash"
+                  type="text"
+                  value={txHash}
+                  onChange={handleTxHashChange}
+                  className="flex-1 mr-5 px-2 py-2 border rounded-lg"
+                />
+
+              </div>
+              {status ? (
+                <div className="mt-4">
+                  <p>Status: {status.status}</p>
+                  {status.gasPaidInfo && (
+                    <p>Gas Paid Status: {status.gasPaidInfo.status}</p>
+                  )}
+                  {status.errors && <p>Errors: {JSON.stringify(status.errors)}</p>}
+                  {status.callData && (
+                    <p>Call Data: {JSON.stringify(status.callData)}</p>
+                  )}
+                </div>
+              ) : (
+                <p className="mt-4">Enter a transaction hash to see its status</p>
+              )}
+            </div>
+          )}
+            
+            <div className="my-10 text-white">
+                <WalletAddress address={address} />
+            </div>
+        
+            </div>
           </div>
         </div>
+
+
+        <div className="my-10 ">
+        <Link href="https://github.com/vaderlock/Nomad"
+          target="_blank" 
+          rel="noopener noreferrer">
+            <BsGithub size={50}/>
+          </Link>
+        </div>
+
+        <div className="my-10 ">
+        <p className='text-center'>Copyright &copy; 2023 Nomad. All Rights Reserved</p>
+        </div>
+
+
+
+      {/* <Web3Button
+        contractAddress="{{contract_address}}"
+        action={async (contract) => contract.call("myFunctionName")}
+      >
+        Button
+
+      </Web3Button> */}
+
+      {/* <Web3Button
+        contractAddress="0x..."
+        action={(contract) => console.log(contract)} // Logic to execute when clicked
+        onSubmit={() => console.log("Transaction submitted")} //Callback function to be run after the user has confirmed the transaction.
+        onSuccess={(result) => alert("Success!")} //Callback function to be run when the contract method call is successful.
+        onError={(error) => alert("Something went wrong!")} //Callback function to be run when the contract method call fails.
+      >
+        Create Social Profile
+      </Web3Button> */}
+
+      {/* <ThirdwebNftMedia 
+      metadata={nft.metadata} 
+      width={500}
+      height={500}
+
+      /> */}
+
+
+
+        </main>
+
+
       </div>
-
-
-      <div className="my-10 ">
-      <Link href="https://github.com/vaderlock/Nomad"
-        target="_blank" 
-        rel="noopener noreferrer">
-          <BsGithub size={50}/>
-        </Link>
-      </div>
-
-      <div className="my-10 ">
-      <p className='text-center'>Copyright &copy; 2023 Nomad. All Rights Reserved</p>
-      </div>
-
-
-
-    {/* <Web3Button
-      contractAddress="{{contract_address}}"
-      action={async (contract) => contract.call("myFunctionName")}
-    >
-      Button
-
-    </Web3Button> */}
-
-    {/* <Web3Button
-      contractAddress="0x..."
-      action={(contract) => console.log(contract)} // Logic to execute when clicked
-      onSubmit={() => console.log("Transaction submitted")} //Callback function to be run after the user has confirmed the transaction.
-      onSuccess={(result) => alert("Success!")} //Callback function to be run when the contract method call is successful.
-      onError={(error) => alert("Something went wrong!")} //Callback function to be run when the contract method call fails.
-    >
-      Create Social Profile
-    </Web3Button> */}
-
-    {/* <ThirdwebNftMedia 
-    metadata={nft.metadata} 
-    width={500}
-    height={500}
-
-    /> */}
-
-
-
-      </main>
+      
+     
     </>
   )
 }
