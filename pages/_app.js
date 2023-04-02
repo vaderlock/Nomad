cf39b6aff80d43f286850e7e04a054458a905394
vaderlock <smuntase@nyit.edu>
2023-04-02 03:55:05 -0400

    fixed create social identity button+removed title

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import "../styles/globals.css";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "goerli";
 
 function MyApp({ Component, pageProps }) {
   return (
