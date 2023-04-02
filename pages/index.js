101da04324a4c5e0a17c4fc403402b30e3ade9e5
vaderlock <smuntase@nyit.edu>
2023-04-02 06:17:45 -0400

    wallet checker

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,17 @@ export default function Home() {
   const [status, setStatus] = useState(null);
   const [txHash, setTxHash] = useState("");
 
+  const [claimed, setClaimed] = useState(false);
+
+  const handleClaim = async (contract) => {
+    try {
+      await contract.erc721.claim(1);
+      setClaimed(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const handleTxHashChange = (event) => {
     setTxHash(event.target.value);
   };
@@ -92,6 +103,7 @@ export default function Home() {
 
         <div className='items-center justify-center m-auto glass pt-30'>
           <div className="grid grid-cols-1 text-center mt-10 mx-5 text-center m-auto">
+            
             <div className="max-w-md m-auto">
 
               {/* <h1 className="text-3xl font-bold pb-10 text-white">Nomad</h1> */}
@@ -106,19 +118,57 @@ export default function Home() {
 
               {address && (
 
-                <Web3Button
-                contractAddress="0xb413df01580659F671471956e9D2fAe989d1dcd3"
-                action={(contract) => contract.erc721.claim(1)}
-                className="custom-web3-button"
-              >
-                Create Social Identity
-              </Web3Button>
+             <Web3Button
+              contractAddress="0xb413df01580659F671471956e9D2fAe989d1dcd3"
+              action={handleClaim}
+              className="custom-web3-button"
+            >
+              Cross Chain Social Identity
+            </Web3Button>
 
-              )}
+              )}    
 
+          </div>
+
+          {claimed && (
+        
+        <div class="bg-gray-200 p-4 rounded-lg flex items-center space-x-4">
+          <div class="w-16 h-16 rounded-full overflow-hidden">
+                <Image
+                  src="/pfp.png"
+                  alt="Profile Picture"
+                  class="w-full h-full object-cover"
+                  width={100}
+                  height={100}
+                />
               </div>
+              <div>
+                <h2 class="font-bold text-lg">Vaderlock</h2>
+                <p class="text-gray-700 text-sm">NFT Degen</p>
+              </div>
+            </div>
+          )}
             
-          {address && (
+            <div className="my-10 text-white">
+                <WalletAddress address={address} />
+            </div>
+            <div className='grid grid-cols-4 justify-center items-center align-center text-center mx-auto gap-0'>
+              <h1>Powered by: </h1>
+            <Image className="ml-5" src="/third.png" alt="logo" width={200} height={200} />
+            <h1 className='pl-5'> and </h1>
+         
+           
+            <Image className="pr-5" src="/axelar.png" alt="logo" width={100} height={140} />
+     
+
+            </div>        
+         
+            </div>
+
+          </div>
+      
+        </div>
+
             <div className="my-10 text-white">
               <label htmlFor="txHash" className="block mb-2">
                 Enter a transaction hash to see its status
@@ -148,29 +198,6 @@ export default function Home() {
                 <p className="mt-4">TX HASH STATUS</p>
               )}
             </div>
-          )}
-            
-            <div className="my-10 text-white">
-                <WalletAddress address={address} />
-            </div>
-            <div className='grid grid-cols-4 justify-center items-center align-center text-center mx-auto gap-0'>
-              <h1>Powered by: </h1>
-            <Image className="ml-5" src="/third.png" alt="logo" width={200} height={200} />
-            <h1 className='pl-5'>and </h1>
-         
-           
-            <Image className="pr-5" src="/axelar.png" alt="logo" width={100} height={140} />
-     
-
-
-            </div>
-        
-         
-            </div>
-          </div>
-      
-        </div>
-
 
         <div className="my-10 ">
         <Link href="https://github.com/vaderlock/Nomad"
